4c1f7775ac9898d4c5025d7be13846b23ccbef20
Satyam Shree <satyamshree780@gmail.com>
2022-03-05 23:48:40 +0530

    s

--- Shorty/settings.py
+++ Shorty/settings.py
@@ -173,5 +173,8 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-CORS_ALLOW_ALL_ORIGINS = True # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
-CORS_ALLOW_CREDENTIALS = False
+# CORS_ALLOW_ALL_ORIGINS = True # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
+# CORS_ALLOW_CREDENTIALS = False
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",'https://shorty-fe-satya-sudo.vercel.app'
+]
