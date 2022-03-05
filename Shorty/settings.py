b0d402afe0059b95a09ecedf1c13ff9111961cca
Satyam Shree <satyamshree780@gmail.com>
2022-03-05 23:38:01 +0530

    sd

--- Shorty/settings.py
+++ Shorty/settings.py
@@ -175,6 +175,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
 CORS_ALLOW_CREDENTIALS = False
-CORS_ALLOWED_ORIGINS = [
-    '*'
-]
