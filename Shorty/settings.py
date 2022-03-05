d126456a289ddabe94707b4cd6bbe4d633990715
Satyam Shree <satyamshree780@gmail.com>
2022-03-05 22:17:05 +0530

    minor error fixing

--- Shorty/settings.py
+++ Shorty/settings.py
@@ -174,8 +174,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # CORS_ALLOW_ALL_ORIGINS = True # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000','https://shorty-fe.vercel.app/'
+    'http://localhost:3000','https://shorty-fe.vercel.app'
 ] # If this is used, then not need to use `CORS_ALLOW_ALL_ORIGINS = True`
 CORS_ALLOWED_ORIGIN_REGEXES = [
-    'http://localhost:3000','https://shorty-fe.vercel.app/'
+    'http://localhost:3000','https://shorty-fe.vercel.app'
 ]
